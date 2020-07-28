4d1882e34e27af2686a3befda39dd2b684d76693
Krzysztof Gumuła <k.gumula@icloud.com>
2020-07-28 18:40:55 +0200

    add handlebars

--- server.js
+++ server.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const path = require('path');
+const hbs = require('express-handlebars');
 
 const app = express();
+
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
 
 app.use((req, res, next) => {
     res.show = (name) => {
@@ -12,24 +16,28 @@ app.use((req, res, next) => {
 
 app.use(express.static(path.join(__dirname, '/public')));
 
-app.use('/user', (req, res) => {
-    res.show('forbidden.html');
+app.get('/', (req, res) => {
+    res.render('index', { layout: false });
 });
 
-app.get('/', (req, res) => {
-    res.show('index.html');
+app.get('/hello/:name', (req, res) => {
+    res.render('hello', { layout: false, name: req.params.name });
+  });
+
+app.use('/user', (req, res) => {
+    res.render('forbidden', { layout: false });
 });
 
 app.get('/home', (req, res) => {
-    res.show('index.html');
+    res.render('index', { layout: false });
 });
 
 app.get('/about', (req, res) => {
-    res.show('about.html');
+    res.render('about', { layout: false });
 });
 
 app.use((req, res) => {
-    res.status(404).show('not_found.html');
+    res.status(404).render('not_found', {layout: false});
 });
 
 app.listen(8000, () => {
