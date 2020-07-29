74c286f8228785ad34a733504c6e58a208880338
Krzysztof Gumuła <k.gumula@icloud.com>
2020-07-29 17:37:16 +0200

    add new styles & POSTMAN

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
+const multer = require('multer');
+const upload = multer();
 
 const app = express();
 
 app.engine('.hbs', hbs());
 app.set('view engine', '.hbs');
 
+app.use(express.urlencoded({
+    extended: false
+}));
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
     res.render('index');
 });
 
-  app.get('/about', (req, res) => {
-    res.render('about', {layout: 'dark'});
+app.get('/about', (req, res) => {
+    res.render('about', {
+        layout: 'dark'
+    });
 });
 
 app.get('/contact', (req, res) => {
@@ -30,8 +37,21 @@ app.get('/history', (req, res) => {
 });
 
 app.get('/hello/:name', (req, res) => {
-    res.render('hello', { layout: false, name: req.params.name });
-  });
+    res.send(`Hello ${req.params.name}`);
+});
+
+app.post('/contact/send-message', upload.single('image'), (req, res) => {
+
+    const {author, sender, title, message} = req.body;
+
+    if(author && sender && title && message && req.file) {
+        res.render('contact', { isSent: true, imageName: req.file.originalname });
+      }
+      else {
+        res.render('contact', { isError: true });
+      }
+
+});
 
 app.use((req, res) => {
     res.status(404).render('not_found');
